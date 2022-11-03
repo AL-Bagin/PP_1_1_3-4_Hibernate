7aedc2638594599ecf7c883df776c61d907626e2
AL-Bagin <mr.alex.bagin@mail.ru>
2022-11-04 01:15:16 +0300

    fix Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,25 +3,30 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl();
-        userService.createUsersTable();
-        User user1 = new User("Александр", "Багин", (byte) 24);
-        User user2 = new User("Иван", "Иванов", (byte) 20);
-        User user3 = new User("Петр", "Петров", (byte) 31);
-        User user4 = new User("Алексей", "Федоров", (byte) 25);
-        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
-        System.out.println("User с именем - " + user1.getName() + " добавлен в базу данных");
-        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
-        System.out.println("User с именем - " + user2.getName() + " добавлен в базу данных");
-        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
-        System.out.println("User с именем - " + user3.getName() + " добавлен в базу данных");
-        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
-        System.out.println("User с именем - " + user4.getName() + " добавлен в базу данных");
-        System.out.println(userService.getAllUsers());
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        try {
+            UserService userService = new UserServiceImpl();
+            userService.createUsersTable();
+            User user1 = new User("Александр", "Багин", (byte) 24);
+            User user2 = new User("Иван", "Иванов", (byte) 20);
+            User user3 = new User("Петр", "Петров", (byte) 31);
+            User user4 = new User("Алексей", "Федоров", (byte) 25);
+            userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+            System.out.println("User с именем - " + user1.getName() + " добавлен в базу данных");
+            userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+            System.out.println("User с именем - " + user2.getName() + " добавлен в базу данных");
+            userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+            System.out.println("User с именем - " + user3.getName() + " добавлен в базу данных");
+            userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+            System.out.println("User с именем - " + user4.getName() + " добавлен в базу данных");
+            System.out.println(userService.getAllUsers());
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
+        } finally {
+            Util.getSessionFactory().close();
+        }
     }
 }
