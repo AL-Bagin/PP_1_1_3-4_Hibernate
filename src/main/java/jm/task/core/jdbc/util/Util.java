475aa044562a6120d7403cb6f2fd8759a76492f3
AL-Bagin <mr.alex.bagin@mail.ru>
2022-11-04 00:24:24 +0300

    addHibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+    private static SessionFactory sessionFactory;
     private static String DB_URL = "jdbc:mysql://localhost:3306/jdbc-hibernate";
     private static String DB_USERNAME = "root";
     private static String DB_PASSWORD = "root";
@@ -17,6 +24,29 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
 
+                Properties settings = new Properties();
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/jdbc-hibernate");
+                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (HibernateException e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
